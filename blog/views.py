13c1a1566c4bf1ec2fc6ec99a56b10ac3c05b205
VitalR <vitalii.rodikov@gmail.com>
2020-07-03 19:34:20 +0300

    Add dbsqlite3 and migration

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-	{
-		'author': 'John Snow',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'July 2, 2020'
-	},
-	{
-		'author': 'Drew Harley',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'July 3, 2020'
-	}
-]
+from .models import Post
 
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 	
