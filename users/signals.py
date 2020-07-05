8ba0a678c50522a219636a55c87207f8d5d58e4d
VitalR <vitalii.rodikov@gmail.com>
2020-07-05 18:07:21 +0300

    Add update user profile functionality

--- users/signals.py
+++ users/signals.py
@@ -1,14 +1,21 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.dispatch import receiver
 from .models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
+def save_or_create_profile_for_existing_user(sender, instance, created, *args, **kwargs):
+    try:
+        if created:
+            instance.profile.save()
+    except Profile.DoesNotExist:
         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    # if created:
+    #     Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_profile(sender, instance, **kwargs):
+#     instance.profile.save()
